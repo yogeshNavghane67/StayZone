f309f6a8e2306c0bf9aee65138e088a941fda732
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-03-29 23:51:59 +0530

    User Registration API

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,10 +1,24 @@
 import express, {Request, Response} from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import { check, validationResult} from "express-validator";
 
 const router = express.Router();
 
-router.post("/register", async (req: Request, res: Response) => {
+
+// /api/users/register
+router.post("/register", [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({ min:6,
+    }),
+
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ message: errors.array() });
+    }
     try {
         let user = await User.findOne({
             email: req.body.email,
