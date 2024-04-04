751f78c5d1c4df2d8045c8480709b8ec9be3e276
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-04 23:58:36 +0530

    Sign out

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -54,6 +54,13 @@ router.post("/login", [
 router.get("/validate-token", verifyToken, (req: Request, res: Response)=>{
     res.status(200).send({userID: req.userId})
 
-})
+});
+
+router.post("/logout", (req: Request, res: Response)=> {
+    res.cookie("auth_token", "",{
+        expires: new Date(0),
+    });
+    res.send();
+});
 
 export default router;
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,8 +1,8 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from '../api-client';
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
  export type SignInFormData = {
@@ -13,6 +13,7 @@ import { useNavigate } from "react-router-dom";
 const SignIn = () => {
     const {showToast} = useAppContext();
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
 
     const { register, formState: {errors},
     handleSubmit,
@@ -21,7 +22,8 @@ const SignIn = () => {
 
  const mutation = useMutation(apiClient.signIn, {
     onSuccess: async () => {
-        showToast({message: "Sign in Successful!", type: "SUCCESS"})
+        showToast({message: "Sign in Successful!", type: "SUCCESS"});
+        await queryClient.invalidateQueries("validateToken");
         navigate("/");
 
     }, onError:(error: Error)=>{
@@ -64,7 +66,10 @@ const SignIn = () => {
             <span className="text-red-500">{errors.password.message}</span>
           )}
       </label>
-      <span>
+      <span className="flex items-center justify-between">
+        <span className="text-sm">
+          Not Register ? {" "}<Link className="underline" to="/register">Create an account here</Link>
+        </span>
         <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">Login</button>
       </span>
         </form>
