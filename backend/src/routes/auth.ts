eb15ff53fb15b75df0a880e6c1a38ed16849b7e1
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-04 21:03:23 +0530

    Checking user login state

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,8 @@ import { check, validationResult } from "express-validator";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import verifyToken from "../middleware/auth";
+
 
 const router = express.Router();
 
@@ -47,6 +49,11 @@ router.post("/login", [
         console.log(error);
         res.status(500).json({ message: " Somethins went wrong"});
     }
+});
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response)=>{
+    res.status(200).send({userID: req.userId})
+
 })
 
 export default router;
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,33 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () => {
-    return(
-        <div className="bg-blue-800 py-6">
-            <div className="container mx-auto flex justify-between">
-                <span className="text-3xl text-white font-bold tracking-tight">
-                <Link to="/">StayZone.com</Link>
-                </span>
-                <span className="flex space-x-2">
-                    <Link to="/sign-in" className="flex bg-white items-center text-blue-600 pk-3 font-bold hover:bg-gray-100">Sign In</Link>
-                </span>
-            </div>
-        </div>
-    )
-}
+  const { isLoggedIn } = useAppContext();
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">StayZone.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-bookings">My Bookings</Link>
+              <Link to="/my-hotels">My Hotels</Link>
+              <button>Sign out</button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex bg-white items-center text-blue-600 pk-3 font-bold hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
